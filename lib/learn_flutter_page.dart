13712124bedb96257bdbba3a2db6ea61a0a4c1cf
Paulo Filho <pcf.feat@gmail.com>
2022-08-06 18:59:23 -0400

    feat: add action buttons in the appbar to the learn flutter page

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -23,6 +23,14 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
           },
           icon: const Icon(Icons.arrow_back_ios),
         ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              debugPrint('Actions');
+            },
+            icon: const Icon(Icons.info_outline),
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
